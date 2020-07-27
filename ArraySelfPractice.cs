d3c39c67993c52fd18a6365f7d679d4ba3f27f8b
iggytan <67040843+iggytan@users.noreply.github.com>
2020-07-27 21:21:50 +1200

    Add files via upload

--- ArraySelfPractice.cs
+++ ArraySelfPractice.cs
@@ -1,7 +1,39 @@
+/* Various elements to display from */
 Int[] array1={5,10,9,1,7}
 String[] array2={Test,Bless,Chess,Mess,West}
 Floating[] array3={1.1,1.2,1.3,1.4,1.5}
 
+/* Looks like a class to display from? */
 Int[]={5,10,9,1,7}
 
+/* Thought this would display 5 from Int */
 arrayOfInt[0]
+
+/* Think this is the for loop to display all arrays */
+
+
+
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+/* Looks like a class to display from? */
+int[] blah = new int[5];
+
+/* Thought this would display 5 from Int */
+blah[0] = 11;
+blah[1] = 54;
+blah[2] = 31;
+blah[3] = 74;
+blah[4] = 95;
+
+
+int[] blahblah = {1, 3, 5, 7, 9};
+Console.WriteLine(blahblah[0]);
+
+Console.WriteLine(blah[0]);
+
+
+  }
+}
